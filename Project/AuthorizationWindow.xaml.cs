5528293b89284793afda6f816e595ddd2a65d864
kurdupl1k <kapattu333@gmail.com>
2020-04-04 01:30:48 +0300

    some additions

--- Project/AuthorizationWindow.xaml.cs
+++ Project/AuthorizationWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using MailKit.Net.Imap;
+using System.Windows;
 
 namespace Project
 {
@@ -13,6 +14,9 @@ namespace Project
     {
       Email = txtbxEmail.Text;
       Password = psswrdbx.Password;
+
+      new MainWindow().Show();
+      Close();
     }
   }
 }
--- Project/MainWindow.xaml.cs
+++ Project/MainWindow.xaml.cs
@@ -1,10 +1,31 @@
-﻿using System.Windows;
+﻿using MailKit.Net.Imap;
+using MimeKit;
+using System.Windows;
 
 namespace Project
 {
   public partial class MainWindow : Window
   {
     public MainWindow() { InitializeComponent(); }
-   
+
+    private void DataGrid_Loaded(object sender, RoutedEventArgs e)
+    {
+      using (ImapClient imapClient = new ImapClient())
+      {
+        imapClient.Connect("imap.gmail.com", 993, true);
+        imapClient.Authenticate(AuthorizationWindow.Email,
+          AuthorizationWindow.Password);
+
+        var inbox = imapClient.Inbox;
+        inbox.Open(MailKit.FolderAccess.ReadOnly);
+
+        foreach (var message in inbox)
+          dgMessages.Items.Add(new MimeMessage()
+          {
+            Subject = message.Subject,
+            Body = message.Body
+          });
+      }
+    }
   }
 }
